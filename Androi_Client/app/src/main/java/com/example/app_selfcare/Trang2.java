032d6a8357b0f4ccb019cab3e4ea03f9408c5a93
vpyy <phucpro170305@gmail.com>
2025-10-26 17:38:40 +0700

    Fmerge

---
+++ Androi_Client/app/src/main/java/com/example/app_selfcare/Trang2.java
@@ -0,0 +1,24 @@
+package com.example.app_selfcare;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Trang2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_trang2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.trang2), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
